2e76b3ea2b82d3ff565b4b763352c67131340ead
moaazhassan4819 <moaazrao5819@gmail.com>
2025-12-19 00:15:57 +0500

    first commit

---
+++ assignment-01-alert/script.js
@@ -0,0 +1,13 @@
+alert("Wellcome");
+
+alert("JavaScript Alert \nError! Please Enter a valid password.");
+
+alert('Welcome to JS Land... \nHappy Coding!');
+
+alert("Welcome to JS Land...");
+
+alert('Happy Coding! \nPrevent this page from creating additional');
+
+alert("Hello... I can run JS through my web browser's console.");
+
+
---
+++ assignment-02-variable-string/app.js
@@ -0,0 +1,32 @@
+var username;
+
+var myName = "Moaaz Hassan";
+
+var message;
+message = "Hello World";
+
+alert(message);
+
+var studentName = "Jhone Doe";
+alert("Name : " + studentName);
+
+var age = 18;
+alert("Age " + age);
+
+var certified = "Certified Mobile Application Development";
+alert(certified);
+
+var pizza =  "Pizza \nPizz \nPiz \nPi \nP";
+alert(pizza);
+
+var address = 'My email address is example ';
+var email = "xyz@gmail.com";
+alert("Email : " + address + email);
+
+var book = ('A smarter way to learn JavaScript.')
+alert("I am trying to learn from the Book " + book);
+
+document.write("Yah! I can write html content through JavaScript.");
+
+var specialString = "▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(specialString);
